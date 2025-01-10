399e571afe694131c3a96f70f6a2d4e9069f6a24
kumarmahesh3023 <73269819+kumarmahesh3023@users.noreply.github.com>
2025-01-10 23:47:35 +0530

    Initial Commit

---
+++ MealMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealMate
+//
+//  Created by Mahesh Kumar on 10/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealMate/MealMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMateApp.swift
+//  MealMate
+//
+//  Created by Mahesh Kumar on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MealMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
